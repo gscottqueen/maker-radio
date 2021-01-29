2f3a2b730b3a3e259c69f0750c951a69808d0ceb
gscottqueen <geoffreysqueen@gmail.com>
2021-01-29 11:08:09 -0500

    updated some landing styles

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SpotifyButton from './SpotifyButton'
 
 class Landing extends Component {
 
@@ -12,44 +13,55 @@ class Landing extends Component {
     <div class="container">
       <div id="login">
         <div class="title"
-        // style="position: absolute; margin: 0 auto; text-align: center;"
+        style={{
+          position: 'absolute',
+          margin: '0 auto',
+          textQlign: 'center',
+        }}
         >
         <h1
-        // style="font-family: 'Six Caps', sans-serif; font-weight: normal; line-height: 1; margin: 0;"
+          style={{
+            fontFamily: 'Six Caps, sans-serif',
+            fontWeight: 'normal',
+            lineHeight: 1,
+            margin: 0,
+          }}
         >MAKER RADIO</h1>
         <h2
-        // style="font-weight: normal;"
+          style={{
+            fontWeight: 'normal',
+          }}
         >Scientifically proven to make you smarter.</h2>
         </div>
         <div id="spotify-button"
-        // style="position : absolute; right: 20px; bottom: 20px;"
+          style={{
+            position: 'absolute',
+            right: '20px',
+            bottom: '20px'
+          }}
         >
         <div class="arrow"
-        // style="transform: rotate(258deg); position: absolute;right: 70px;top: -120px;"
+        style={{
+          transform: 'rotate(258deg)',
+          position: 'absolute',
+          right: '70px',
+          top: '-120px'
+          }}
         >
           <div class="greeting"
-          // style="font-family: 'Rock Salt', cursive; transform: rotate(90deg);"
+          style={{
+            fontFamily: 'Rock Salt, cursive',
+            transform: 'rotate(90deg)'
+          }}
           >Welcome my dudes!</div>
-          <img src="arrow.png"
-          // style="-webkit-transform: scaleX(-1); transform: scaleX(-1); width: 100px;"
+          <img src="arrow.png" alt="arrow"
+          style={{
+            transform: 'scaleX(-1)',
+            width: '100px'
+          }}
           />
         </div>
-        <div
-        // style="display: flex; justify-content: space-around; align-items: flex-start; align-content: space-between;"
-        >
-          <a href="http://localhost:8888/login"
-          style={{ color: "black"}}
-          // style=" background-image: none; background-color: #1DB954; border-color: #1aa34a; border-radius: 10px; border-width: 0; padding: 5px; cursor: pointer; user-select: none;"
-          > login
-            {/* <img src="Spotify_Icon_RGB_White.png"
-            // style={{
-            //   width: 35px,
-            //   height: 35px
-            //   margin: 5px;border-radius: 50px; border: 4px solid #1aa34a;
-            //   }}
-              /> */}
-          </a>
-        </div>
+        <SpotifyButton />
         </div>
       </div>
     </div>
